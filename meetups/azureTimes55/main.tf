450de5a0e12a4ddc73618d97fb86eda24a3ead56
Maksim Rotar <Maksim_Rotar@epam.com>
2023-01-14 23:08:58 +0300

    init

--- meetups/azureTimes55/main.tf
+++ meetups/azureTimes55/main.tf
@@ -11,12 +11,12 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_managed_disk" "md-ultra" {
-    
+
     name = var.disk.name
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     storage_account_type = var.disk.disk_sku
-    create_option = var.disk.create_option
+    create_option = var.disk.creation_option 
     disk_size_gb = var.disk.size
   
 }
