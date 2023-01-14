b7f3f81224cda8f693a8945e62f95cb4bc80610b
Maksim Rotar <Maksim_Rotar@epam.com>
2023-01-14 22:35:47 +0300

    init

--- meetups/azureTimes55/main.tf
+++ meetups/azureTimes55/main.tf
@@ -1,6 +1,22 @@
 resource "azurerm_resource_group" "rg" {
-    
+
     name = var.rgname.name
     location = var.rgname.location
+
+    tags = {
+      "purpose" = "azureTimes55"
+      "Test"    = "test"
+    }
+  
+}
+
+resource "azurerm_managed_disk" "md-ultra" {
+    
+    name = var.disk.name
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    storage_account_type = var.disk.disk_sku
+    create_option = var.disk.create_option
+    disk_size_gb = var.disk.size
   
 }
--- meetups/azureTimes55/variable.tf
+++ meetups/azureTimes55/variable.tf
@@ -1,15 +1,42 @@
+variable azuretimes {
+    description = "Azure Times Meetup Details"
+    type        = string
+    meetup      = "azuretimes55"
+    location    = "westeurope"
+
+}
+
+
+
 variable "rgname" {
 
     description = "Azure Resource Group Name"
 
     type = object ({
-        name = string
+        name     = string
         location = string
     })
     
-    default = {
-        name        = "azureTimes55"
-        location    = "westeurope"
-    }
+    name     = var.azuretimes.meetup
+    location = var.azuretimes.location
+
+
+}
+
+variable "disk" {
+
+    description = "Azure Managed Disk Config"
+
+    type = object ({
+        name     = string
+        disk_sku = string
+        creation_option = string
+        size = string
+    })
+    
+    name            = var.azuretimes.meetup
+    disk_sku        = "UltraSSD_LRS"
+    creation_option = "Empty"
+    size            = "100"
 
 }
