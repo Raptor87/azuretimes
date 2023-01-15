852d04a3191a8f811648aa50ca57305f7ebee45a
Maksim Rotar <Maksim_Rotar@epam.com>
2023-01-15 16:36:55 +0300

    init

--- meetups/azureTimes55/main.tf
+++ meetups/azureTimes55/main.tf
@@ -1,5 +1,4 @@
 module "rg" {
-  for_each = { for rg in var.rg_list : rg.name => rg }
   source = "../../Terraform/modules/azurerm_rg"
   name = "azuretimes55"
   location = "westeurope"
@@ -10,7 +9,6 @@ module "rg" {
 
 
 resource "azurerm_managed_disk" "md-ultra" {
-    for_each = { for disk in var.disk_list : disk.disk_name => disk }
     name = "test"
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
