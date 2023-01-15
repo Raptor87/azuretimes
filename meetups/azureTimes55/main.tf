e42f7006670f3fbe084307035ed7ccce19358795
Maksim Rotar <Maksim_Rotar@epam.com>
2023-01-15 16:35:58 +0300

    init

--- meetups/azureTimes55/main.tf
+++ meetups/azureTimes55/main.tf
@@ -1,20 +1,21 @@
 module "rg" {
   for_each = { for rg in var.rg_list : rg.name => rg }
   source = "../../Terraform/modules/azurerm_rg"
-  name = var.value.name
-  location = var.value.location
-  tags = var.value.tags
-
+  name = "azuretimes55"
+  location = "westeurope"
+    tags = {
+        name = "name"
+        }
 }
 
 
 resource "azurerm_managed_disk" "md-ultra" {
     for_each = { for disk in var.disk_list : disk.disk_name => disk }
-    name = var.value.disk_name
+    name = "test"
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
-    storage_account_type = var.value.disk_sku
-    create_option = var.value.disk_creation_option 
-    disk_size_gb = var.value.disk_size
+    storage_account_type = "UltraSSD_LRS"
+    disk_size_gb = "124"
+
   
 }
--- meetups/azureTimes55/variables.tf
+++
@@ -1,2 +0,0 @@
-variable "rg_list" {}
-variable "diks_list" {}
