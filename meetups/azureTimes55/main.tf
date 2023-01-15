f2dee4e9a4e2cf26a941a335a1aab09763bc998f
Maksim Rotar <Maksim_Rotar@epam.com>
2023-01-15 16:09:57 +0300

    init

--- meetups/azureTimes55/main.tf
+++ meetups/azureTimes55/main.tf
@@ -1,20 +1,20 @@
 module "rg" {
 
   source = "../../Terraform/modules/azurerm_rg"
-  name = var.rgname.name
-  location = var.rgname.location
-  tags = var.rgname.tags
+  name = var.name
+  location = var.location
+  tags = var.tags
 
 }
 
 
 resource "azurerm_managed_disk" "md-ultra" {
 
-    name = var.disk.name
+    name = var.disk_name
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
-    storage_account_type = var.disk.disk_sku
-    create_option = var.disk.creation_option 
-    disk_size_gb = var.disk.size
+    storage_account_type = var.disk_sku
+    create_option = var.disk_creation_option 
+    disk_size_gb = var.disk_size
   
 }
