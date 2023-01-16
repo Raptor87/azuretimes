38530635b522d74a70887b0ff0e21725d73dbc92
Maksim Rotar <Maksim_Rotar@epam.com>
2023-01-16 08:02:57 +0300

    init

--- meetups/azureTimes55/main.tf
+++ meetups/azureTimes55/main.tf
@@ -11,7 +11,7 @@ module "rg" {
 resource "azurerm_managed_disk" "md-ultra" {
     name = "test"
     location = module.rg.location
-    resource_group_name = module.rg..name
+    resource_group_name = module.rg.name
     storage_account_type = "UltraSSD_LRS"
     disk_size_gb = "124"
 }
